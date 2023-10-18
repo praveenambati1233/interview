51c9ce6864b092e600a4c6acb89f51183dd03847
Praveen Ambati <49944591+praveenambati1233@users.noreply.github.com>
2023-10-18 17:05:27 +0530

    sample java program

---
+++ tax-calculator/src/main/java/com/taxcalculator/employee/bean/Employee.java
@@ -0,0 +1,58 @@
+package com.taxcalculator.employee.bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public final class Employee {
+
+    private Integer id;
+
+    private String name;
+
+    private String address;
+
+    private Double Salary;
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Double salary) {
+        Salary = salary;
+    }
+}
---
+++ tax-calculator/src/main/java/com/taxcalculator/employee/service/ExceptionHandling.java
@@ -0,0 +1,16 @@
+package com.taxcalculator.employee.service;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * This class contains the business logic to throw an exception
+ */
+public final class ExceptionHandling {
+
+    public String convertIntoUpperCase(String input) {
+        if (StringUtils.isEmpty(input)) {
+            throw new IllegalArgumentException("Empty value is passed.");
+        }
+        return input.toUpperCase();
+    }
+}
